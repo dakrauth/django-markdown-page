674cf9964281a4f74a0004cdfab63157b17b6224
David Krauth <dakrauth@gmail.com>
2019-08-10 11:37:04 -1000

    fix is_authenticated

--- mdpage/utils.py
+++ mdpage/utils.py
@@ -6,8 +6,8 @@ from markdown2 import Markdown
 from django.contrib.auth.decorators import user_passes_test, login_required
 from .settings import get_settings
 
-superuser_required = user_passes_test(lambda u: u.is_authenticated() and u.is_active and u.is_superuser)
-staff_required = user_passes_test(lambda u: u.is_authenticated() and u.is_active and u.is_staff)
+superuser_required = user_passes_test(lambda u: u.is_authenticated and u.is_active and u.is_superuser)
+staff_required = user_passes_test(lambda u: u.is_authenticated and u.is_active and u.is_staff)
 
 def get_mdp_type_template_list(mdp_type, tmpl_part):
     return [
